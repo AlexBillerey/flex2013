030900e1900e55bbb2ffb08730426c3c50fffd73
AlexBillerey <alex_billerey@hotmail.com>
2013-09-08 00:27:51 +0100

    Initial commit

--- js/bba.js
+++ js/bba.js
@@ -6,34 +6,41 @@
  * To change this template use File | Settings | File Templates.
  */
 function navslider() {
-    //alert('navslider');
+
     "use strict";
     $('a[href^="#"],a[href="#top"]').bind('click.smoothscroll',function (e) {
         e.preventDefault();
         var target = this.hash;
-        $target = $(target);
+        var $target = $(target);
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
-        }, 250, 'linear', function () {
+        }, 500, 'linear', function () {
             window.location.hash = target;
         });
     });
 }
 
 
-function testViewportSize(){
+function testViewportSize() {
     "use strict";
-    if (document.documentElement.clientWidth < 599) {
-        $('section h1').each(function () {
-            $(this).append('<span class="floatRight"><a href="#top">top</a></span>');
-        });
-        navslider();
+    //var headerH = $('header').height();
+
+    if ($(window).scrollTop() > 230) {
+        $('.toTop').fadeIn(500);
     }
+        else {
+            $('.toTop').fadeOut(50);
+        }
 }
 
 
 $(document).ready(function () {
     "use strict";
+    $('section h1').each(function () {
+        $('<span class="toTop"><a href="#top"></a></span>').insertBefore(this);
+    });
+    $(window).scroll(function () {
     testViewportSize();
-
+    });
+    navslider();
 });
